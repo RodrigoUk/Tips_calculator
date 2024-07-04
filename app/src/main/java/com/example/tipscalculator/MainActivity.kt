7c9f9eeb823bb6fd7e3aeade6f9d9356da5f745b
macpromax <rodrigoyki@hotmail.com>
2024-07-04 20:16:13 +0100

    -Button Calculator Snackbar clear. -Call radio buttons -Call layout views -clear -calculator result -show results

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -2,12 +2,24 @@ package com.example.tipscalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import com.example.tipscalculator.databinding.ActivityMainBinding
-import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
+
+    //Valor total da conta
+    //Numero de pessoas
+    //Porcentagem da gorjeta
+    // 10%, 15% or 20%
+    //Calcular
+    //Limpar
+
+    //Recuper as views do layout
+    //Viewbinding
+    //Recuper os radio buttons
+    //Calculo de tips
+    //Mostrar os resultados
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,13 +29,54 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        var percentage = 0
+        binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
+            println("Roque1 Option one:$isChecked")
+            if (isChecked) {
+                percentage = 10
+            }
+        }
 
+        binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
+            println("Roque1 Option Two:$isChecked")
+            if (isChecked) {
+                percentage = 15
+            }
+        }
+
+        binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
+            println("Roque1 Option Three: $isChecked")
+            if (isChecked) {
+                percentage = 20
+            }
+        }
 
         binding.btnClean.setOnClickListener {
             println("Roque1 " + binding.tieTotal.text)
             println("Roque1 " + binding.tieNumPeople.text)
+        }
 
+        binding.btnDone.setOnClickListener {
+            val totalTableTemp = binding.tieTotal.text
+            val nPeopleTemp = binding.tieNumPeople.text
 
+            if (totalTableTemp?.isEmpty() == true ||
+                nPeopleTemp?.isEmpty() == true
+            ) {
+                Snackbar.make(binding.tieTotal, "Please fill in all fields.", Snackbar.LENGTH_LONG)
+                    .show()
+            } else {
+                val totalTable: Float = totalTableTemp.toString().toFloat()
+                val nPeople: Int = nPeopleTemp.toString().toInt()
+
+                val totalTemp = totalTable / nPeople
+                val tips = totalTemp * percentage / 100
+                val totalWithTips = totalTemp + tips
+                "Total with tips: $totalWithTips".also {
+                    binding.tvResult.text = it
+
+                }
+            }
         }
     }
 }
